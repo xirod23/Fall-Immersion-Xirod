e2c2b095f8e928ab0c5d1df708a9e80abc4a982e
xirod23 <xirod.waters@hgs.hiddengeniusproject.org>
2024-09-12 17:29:57 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,9 @@
 function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
-}
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+
+    //create a variable to store the tag with our text we want to change
+    counter_number = document.getElementById('counter-number');
+
+    //chnage the text of our tag, in this case incriment its number
+    counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
+    }
