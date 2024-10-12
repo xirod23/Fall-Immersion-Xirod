1b0b06d990900393c0c32639980557945a4fa59c
xirod23 <xirod.waters@hgs.hiddengeniusproject.org>
2024-10-12 13:11:47 -0700

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,21 @@
-function formSubmission() {
-    // Your code here.
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('myForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        //Add Varibles from index.html (hint: Use document.get function)
+        const name = document.getElementById("name").value;
+        const message = document.getElementById("name").value;
+        const email = document.getElementById("name").value;
+        if (name && email && message) {
+            alert(`Form submitted successfully!\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+        } else {
+            alert('Please fill all the fields');
+        }
+    });
+});
+//Add additional fields
+
+
+
+
+NEW
