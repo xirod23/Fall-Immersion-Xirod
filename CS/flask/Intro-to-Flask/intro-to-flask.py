6a4756084aa7a9f765fe385ed4cea09b25956514
xirod23 <xirod.waters@hgs.hiddengeniusproject.org>
2024-12-04 18:20:21 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,11 @@
-# instruction will below. 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return "Hello World"
+
+
+        if __name__ == '__main__':
+            app.run(debug = True)# instruction will below. 
